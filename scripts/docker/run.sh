ac98a803cd826d4826a09b6db72c77494ff7f807
Freddie Ruxton <ruxton.f@gmail.com>
2021-10-19 19:22:54 +0100

    Mount data and models directories when running tf-lab container

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
 # Start a container using the tensorflow-jupyterlab image,
-# mounting the cwd to the container's /project directory.
+# mounting notebooks, data, models etc to the container's
+# /project directory.
+#
+# Usage: Run from the repo's root directory
+#
+#   ./scripts/docker/run.sh       # launch jupyterlab server
+#   ./scripts/docker/run.sh bash  # enter interactive bash shell, useful for managing dependencies etc
 
-if [ $1 = "bash" ] ; then
-    docker run \
-        --gpus=all \
-        -v "$(pwd)/notebooks:/project/notebooks" \
-        -v "$(pwd)/.jupyter/jupyter_notebook_config.py:/project/.jupyter/jupyter_notebook_config.py" \
-        -p 8888:8888 \
-        --rm -it \
-        tf-lab bash
+flags=(
+    --gpus all \
+    -v "$(pwd)/notebooks:/project/notebooks" \
+    -v "$(pwd)/data:/project/data" \
+    -v "$(pwd)/models:/project/models" \
+    -v "$(pwd)/.jupyter/jupyter_notebook_config.py:/project/.jupyter/jupyter_notebook_config.py" \
+    -p 8888:8888
+)
+
+if [ "$1" = "bash" ] ; then
+    docker run "${flags[@]}" --rm -it tf-lab bash
 else
-    docker run \
-        --gpus=all \
-        -v "$(pwd)/notebooks:/project/notebooks" \
-        -v "$(pwd)/.jupyter/jupyter_notebook_config.py:/project/.jupyter/jupyter_notebook_config.py" \
-        -p 8888:8888 \
-        tf-lab "$@"
+    docker run "${flags[@]}" tf-lab "$@"
 fi
